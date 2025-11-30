063501e266f52de4584dacf6831376585e5226dc
Hyun-je <bigae2@gmail.com>
2025-11-30 19:07:49 +0900

    add conda tos accept

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+conda tos accept --override-channels --channel https://repo.anaconda.com/pkgs/main
+conda tos accept --override-channels --channel https://repo.anaconda.com/pkgs/r
+
 bash install_ola.sh
 bash install_dmx.sh
 bash install_face.sh
