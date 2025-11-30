6237f28acef886827f1bcddac3309f8f47a98178
Hyun-je <bigae2@gmail.com>
2025-11-30 17:20:50 +0900

    fix install

--- install_face.sh
+++ install_face.sh
@@ -7,3 +7,5 @@ conda create -y -n face python=3.9
 /home/pi/miniconda3/envs/face/bin/python -m pip install numpy==1.26.4
 /home/pi/miniconda3/envs/face/bin/python -m pip install python-osc==1.8.3
 /home/pi/miniconda3/envs/face/bin/python -m pip install FaceAnalyzer==0.1.33
+
+sudo bash service/face/install.sh
