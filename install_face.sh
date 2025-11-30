f6b70f6f63b9ee335d50b1b0ec0bb6a56f550f74
Hyun-je <bigae2@gmail.com>
2025-11-30 17:26:50 +0900

    add yaml

--- install_face.sh
+++ install_face.sh
@@ -7,5 +7,6 @@ conda create -y -n face python=3.9
 /home/pi/miniconda3/envs/face/bin/python -m pip install numpy==1.26.4
 /home/pi/miniconda3/envs/face/bin/python -m pip install python-osc==1.8.3
 /home/pi/miniconda3/envs/face/bin/python -m pip install FaceAnalyzer==0.1.33
+/home/pi/miniconda3/envs/face/bin/python -m pip install pyyaml
 
 sudo bash service/face/install.sh
