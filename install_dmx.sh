65e8bf1415453d0194bab3c17206aac213e77fd4
Hyun-je <bigae2@gmail.com>
2025-11-30 19:02:05 +0900

    change install

--- install_dmx.sh
+++ install_dmx.sh
@@ -2,7 +2,7 @@
 
 ola_patch -u 0 -d 8 -p 1
 
-/home/pi/miniconda3/bin/conda create -y -n dmx python=3.8
+conda create -y -n dmx python=3.8
 
 /home/pi/miniconda3/envs/dmx/bin/python -m pip install ola
 /home/pi/miniconda3/envs/dmx/bin/python -m pip install python-osc==1.8.3
