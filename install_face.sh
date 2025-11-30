ab403b75da1c0f51c2d243f29291edbdce0b6b52
Hyun-je <bigae2@gmail.com>
2025-11-30 16:54:14 +0900

    add install_face.sh

---
+++ install_face.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+conda create -y -n face python=3.9
+
+/home/pi/miniconda3/envs/face/bin/python -m pip install mediapipe
+/home/pi/miniconda3/envs/face/bin/python -m pip install opencv-python==4.6.0.66
+/home/pi/miniconda3/envs/face/bin/python -m pip install numpy==1.26.4
+/home/pi/miniconda3/envs/face/bin/python -m pip install python-osc==1.8.3
+/home/pi/miniconda3/envs/face/bin/python -m pip install FaceAnalyzer==0.1.33
